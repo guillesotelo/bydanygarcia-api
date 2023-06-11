ac4ccbe858218a508a85a90b735427bd1015ff77
Guillermo Sotelo <guillermo.sotelo@conexiogroup.com>
2023-06-11 11:16:15 +0200

    updated getAll

--- api/routes/post.js
+++ api/routes/post.js
@@ -5,7 +5,7 @@ const { Post } = require('../db/models')
 //Get all posts
 router.get('/getAll', async (req, res, next) => {
     try {
-        const posts = await Post.find().sort([['createdAt', 'descending']])
+        const posts = await Post.find().sort({ createdAt: -1 })
         if (!posts) return res.status(404).send('No posts found.')
 
         res.status(200).json(posts)
