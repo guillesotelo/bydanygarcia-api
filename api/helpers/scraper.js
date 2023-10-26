d3b002c4dd57f0e8ca8fe2ee81e6c9f92e39bec0
Guillermo Sotelo <guillermo.sotelo@volvocars.com>
2023-10-26 10:50:49 +0200

    testing scrao

--- api/helpers/scraper.js
+++ api/helpers/scraper.js
@@ -1,21 +1,22 @@
 const dotenv = require('dotenv')
 dotenv.config()
-const chromium = require("@sparticuz/chromium");
+const chromium = require("@sparticuz/chromium")
 const fromServer = process.env.AWS_LAMBDA_FUNCTION_VERSION
 puppeteer = fromServer ? require('puppeteer-core') : require('puppeteer')
+const fs = require('fs');
 
 const scrapePage = async (url, selector) => {
     let browser = null
     if (fromServer) {
-        chromium.setHeadlessMode = true;
-        chromium.setGraphicsMode = false;
+        chromium.setHeadlessMode = true
+        chromium.setGraphicsMode = false
 
         browser = await puppeteer.launch({
             ignoreDefaultArgs: ['--disable-extensions'],
-            args: chromium.args,
-            defaultViewport: chromium.defaultViewport,
+            args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+            // defaultViewport: chromium.defaultViewport,
             executablePath: await chromium.executablePath(),
-            headless: chromium.headless,
+            headless: 'always',
             ignoreHTTPSErrors: true
         })
     } else {
@@ -27,42 +28,43 @@ const scrapePage = async (url, selector) => {
         })
     }
 
-    const page = await browser.newPage();
+    const page = await browser.newPage()
 
-    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    await page.goto(url, { waitUntil: 'domcontentloaded' })
 
     // Scroll down repeatedly until all images are loaded
-    let imageUrls = [];
-    let previousHeight;
+    let imageUrls = []
+    let previousHeight
     while (true) {
         const currentHeight = await page.evaluate(() => {
-            window.scrollTo(0, document.body.scrollHeight);
-            return document.body.scrollHeight;
-        });
+            window.scrollTo(0, document.body.scrollHeight)
+            return document.body.scrollHeight
+        })
 
-        if (currentHeight === previousHeight) {
-            break; // If scrolling doesn't load more content, exit the loop
-        }
+        // If scrolling doesn't load more content, exit the loop
+        if (currentHeight === previousHeight) break
 
-        previousHeight = currentHeight;
+        previousHeight = currentHeight
 
         // Extract image URLs after scrolling
         const newImageUrls = await page.evaluate(() => {
-            const images = document.querySelectorAll('img');
+            const images = document.querySelectorAll('img')
             return Array.from(images).map((img) => {
-                const url = img.getAttribute('src');
-                if (url.includes('pinimg') && img.width > 75) return url;
-                else return '';
-            });
-        });
+                const url = img.getAttribute('src')
+                if (url.includes('pinimg') && img.width > 75) return url
+                else return ''
+            })
+        })
 
         // Add the new image URLs to the result
-        imageUrls = [...imageUrls, ...newImageUrls];
+        imageUrls = [...imageUrls, ...newImageUrls]
 
         // Wait for some time before scrolling again
-        await page.waitForTimeout(250); // Adjust this wait time as needed
+        await page.waitForTimeout(250) // Adjust this wait time as needed
     }
-    await browser.close();
+    // [...new Set(imageUrls)].forEach((item, i) => console.log(i, item))
+    // fs.writeFileSync('/home/guillermo/Documents/git/bydanygarcia-api/api/helpers/scrapped.html', await page.content());
+    await browser.close()
 
     return [...new Set(imageUrls)]
 }
