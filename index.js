98830bad4ee623009953a696dc86f0ff5e6e0e52
Guillermo Sotelo <guillermo.sotelo@volvocars.com>
2023-10-27 10:26:45 +0200

    cors

--- index.js
+++ index.js
@@ -12,12 +12,6 @@ app.use(cors({
 }))
 app.use(morgan("dev"))
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
-
 app.use(express.json({ limit: '200mb' }))
 app.use(express.urlencoded({ limit: '200mb', extended: true, parameterLimit: 1000000 }))
 
