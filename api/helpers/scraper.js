2b73d61d4522050bc32fc4a3e7e288c7e21c992d
Guillermo Sotelo <guillermo.sotelo@conexiogroup.com>
2024-02-18 18:56:33 +0100

    try catch

--- api/helpers/scraper.js
+++ api/helpers/scraper.js
@@ -5,53 +5,58 @@ const fromServer = process.env.AWS_LAMBDA_FUNCTION_VERSION
 puppeteer = require('puppeteer-core')
 
 const scrapePage = async (url, selector) => {
-    let browser = null
-    chromium.setHeadlessMode = true
-    chromium.setGraphicsMode = false
+    try {
+        let browser = null
+        chromium.setHeadlessMode = true
+        chromium.setGraphicsMode = false
 
-    browser = await puppeteer.launch({
-        ignoreDefaultArgs: ['--disable-extensions'],
-        args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath(),
-        headless: chromium.headless,
-        ignoreHTTPSErrors: true
-    })
-
-    const page = await browser.newPage()
-    await page.setViewport({
-        width: 1920,
-        height: 1080,
-    })
-    await page.goto(url, { waitUntil: "domcontentloaded" })
-    let imageUrls = []
-    let previousHeight
-
-    while (true) {
-        const currentHeight = await page.evaluate(() => {
-            window.scrollTo(0, document.body.scrollHeight)
-            return document.body.scrollHeight
+        browser = await puppeteer.launch({
+            ignoreDefaultArgs: ['--disable-extensions'],
+            args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath(),
+            headless: chromium.headless,
+            ignoreHTTPSErrors: true
         })
 
-        const newImageUrls = await page.evaluate(() => {
-            const images = document.querySelectorAll("div[role='list']")[0].querySelectorAll('img')
-            return Array.from(images).map((img) => {
-                const url = img.getAttribute('src')
-                if (url.includes('pinimg') && img.width > 75) return url
-                else return ''
+        const page = await browser.newPage()
+        await page.setViewport({
+            width: 1920,
+            height: 1080,
+        })
+        await page.goto(url, { waitUntil: "domcontentloaded" })
+        let imageUrls = []
+        let previousHeight
+
+        while (true) {
+            const currentHeight = await page.evaluate(() => {
+                window.scrollTo(0, document.body.scrollHeight)
+                return document.body.scrollHeight
             })
-        })
 
-        imageUrls = [...imageUrls, ...newImageUrls]
+            const newImageUrls = await page.evaluate(() => {
+                const images = document.querySelectorAll("div[role='list']")[0].querySelectorAll('img')
+                return Array.from(images).map((img) => {
+                    const url = img.getAttribute('src')
+                    if (url.includes('pinimg') && img.width > 75) return url
+                    else return ''
+                })
+            })
 
-        if (currentHeight === previousHeight) break
-        previousHeight = currentHeight
+            imageUrls = [...imageUrls, ...newImageUrls]
 
-        await page.waitForTimeout(250)
+            if (currentHeight === previousHeight) break
+            previousHeight = currentHeight
+
+            await page.waitForTimeout(250)
+        }
+        await browser.close()
+
+        return [...new Set(imageUrls)]
+    } catch (error) {
+        console.error(error)
+        return []
     }
-    await browser.close()
-
-    return [...new Set(imageUrls)]
 }
 
 module.exports = { scrapePage }
