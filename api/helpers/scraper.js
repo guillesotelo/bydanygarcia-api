e6c192734d5f6c9c52b462ebab954e6686197c19
Guillermo Sotelo <guillermo.sotelo@conexiogroup.com>
2024-05-25 19:39:38 +0200

    try-puppeteer-conf

--- api/helpers/scraper.js
+++ api/helpers/scraper.js
@@ -2,9 +2,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 const chromium = require("@sparticuz/chromium")
 const fromServer = process.env.AWS_LAMBDA_FUNCTION_VERSION
-puppeteer = 
-// fromServer ? require('puppeteer-core') : 
-require('puppeteer')
+puppeteer = fromServer ? require('puppeteer-core') : require('puppeteer')
 
 const scrapePage = async (url, selector) => {
     try {
@@ -12,21 +10,23 @@ const scrapePage = async (url, selector) => {
         chromium.setHeadlessMode = true
         chromium.setGraphicsMode = false
 
-        const puppeteerOptions = 
-        // fromServer ?  {
-        //         ignoreDefaultArgs: ['--disable-extensions'],
-        //         args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
-        //         defaultViewport: chromium.defaultViewport,
-        //         executablePath: await chromium.executablePath(),
-        //         headless: chromium.headless,
-        //         ignoreHTTPSErrors: true
-        //     }   :
-             {
-                ignoreDefaultArgs: ['--disable-extensions'],
-                args: ['--hide-scrollbars', '--disable-web-security'],
-                headless: true,
-                ignoreHTTPSErrors: true
-            }
+        const puppeteerOptions =
+            fromServer ?
+                {
+                    ignoreDefaultArgs: ['--disable-extensions'],
+                    args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+                    defaultViewport: chromium.defaultViewport,
+                    executablePath: await chromium.executablePath(),
+                    headless: chromium.headless,
+                    ignoreHTTPSErrors: true
+                }
+                :
+                {
+                    ignoreDefaultArgs: ['--disable-extensions'],
+                    args: ['--hide-scrollbars', '--disable-web-security'],
+                    headless: true,
+                    ignoreHTTPSErrors: true
+                }
 
         browser = await puppeteer.launch(puppeteerOptions)
 
