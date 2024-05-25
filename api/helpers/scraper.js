a69c2d9465a7082722e9efe00f15415888d8d2ec
Guillermo Sotelo <guillermo.sotelo@conexiogroup.com>
2024-05-25 22:04:31 +0200

    test

--- api/helpers/scraper.js
+++ api/helpers/scraper.js
@@ -14,7 +14,7 @@ const scrapePage = async (url, selector) => {
             fromServer ?
                 {
                     ignoreDefaultArgs: ['--disable-extensions'],
-                    args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+                    args: [...chromium.args, '--hide-scrollbars', '--disable-web-security', '--no-sandbox'],
                     defaultViewport: chromium.defaultViewport,
                     executablePath: await chromium.executablePath(),
                     headless: chromium.headless,
