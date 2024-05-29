664ee630cdc8efe2bdc39064a3fe73702e44046f
Guillermo Sotelo <guillermo.sotelo@conexiogroup.com>
2024-05-29 23:24:47 +0200

    test-render

--- api/helpers/scraper.js
+++ api/helpers/scraper.js
@@ -2,9 +2,9 @@ const dotenv = require('dotenv')
 dotenv.config()
 const chromium = require("@sparticuz/chromium")
 const fromServer = process.env.AWS_LAMBDA_FUNCTION_VERSION || process.env.NODE_ENV === 'production'
-puppeteer = 
-fromServer ? require('puppeteer-core') : 
-require('puppeteer')
+puppeteer =
+    // fromServer ? require('puppeteer-core') : 
+    require('puppeteer')
 
 const scrapePage = async (url, selector) => {
     try {
@@ -13,31 +13,31 @@ const scrapePage = async (url, selector) => {
         chromium.setGraphicsMode = false
 
         const puppeteerOptions =
-            fromServer ?
-                {
-                    ignoreDefaultArgs: ['--disable-extensions'],
-                    args: [...chromium.args, '--hide-scrollbars', '--disable-web-security', '--no-sandbox'],
-                    defaultViewport: chromium.defaultViewport,
-                    executablePath: await chromium.executablePath(),
-                    headless: chromium.headless,
-                    ignoreHTTPSErrors: true
-                }
-                :
-                {
-                    ignoreDefaultArgs: ['--disable-extensions'],
-                    args: ['--hide-scrollbars', '--disable-web-security'],
-                    headless: true,
-                    ignoreHTTPSErrors: true
-                }
+        // fromServer ?
+        //     {
+        //         ignoreDefaultArgs: ['--disable-extensions'],
+        //         args: [...chromium.args, '--hide-scrollbars', '--disable-web-security', '--no-sandbox'],
+        //         defaultViewport: chromium.defaultViewport,
+        //         executablePath: await chromium.executablePath(),
+        //         headless: chromium.headless,
+        //         ignoreHTTPSErrors: true
+        //     }
+        //     :
+        {
+            ignoreDefaultArgs: ['--disable-extensions'],
+            args: ['--hide-scrollbars', '--disable-web-security'],
+            headless: true,
+            ignoreHTTPSErrors: true
+        }
 
         browser = await puppeteer.launch(puppeteerOptions)
 
         const page = await browser.newPage()
 
-        await page.setViewport({
-            width: 1920,
-            height: 1080,
-        })
+        // await page.setViewport({
+        //     width: 1920,
+        //     height: 1080,
+        // })
 
         await page.goto(url, { waitUntil: "domcontentloaded" })
         let imageUrls = []
@@ -52,7 +52,7 @@ const scrapePage = async (url, selector) => {
             const newImageUrls = await page.evaluate(() => {
                 const images = document.querySelectorAll("div[role='list']")[0].querySelectorAll('img')
                 console.log(images)
-                
+
                 return Array.from(images).map((img) => {
                     const url = img.getAttribute('src')
                     if (url.includes('pinimg') && img.width > 75) return url
@@ -62,12 +62,12 @@ const scrapePage = async (url, selector) => {
 
             imageUrls = [...imageUrls, ...newImageUrls]
 
-            if (currentHeight === previousHeight) break
+            if (currentHeight === previousHeight || currentHeight > 3000) break
             previousHeight = currentHeight
 
             await page.waitForTimeout(250)
         }
-        
+
         await page.close()
         await browser.close()
 
