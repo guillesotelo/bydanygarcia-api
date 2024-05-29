3f6989d5784ff4f0476b67adc523aacad693d7bd
Guillermo Sotelo <guillermo.sotelo@conexiogroup.com>
2024-05-29 23:07:45 +0200

    debug

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { connection } = require("./api/db")
 const routes = require("./api/routes")
 const app = express()
 const PORT = process.env.PORT || 5000
+const prod = process.env.NODE_ENV === 'production'
 
 app.use(cors({
   origin: '*',
@@ -12,8 +13,7 @@ app.use(cors({
 }))
 
 app.use(function (_, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://www.bydanygarcia.com')
-  res.setHeader('Access-Control-Allow-Origin', 'http:// 127.0.0.1')
+  res.setHeader('Access-Control-Allow-Origin', prod ? 'https://www.bydanygarcia.com' : '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
   res.setHeader('Access-Control-Allow-Credentials', true)
